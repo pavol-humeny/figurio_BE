0f04ccf8e6dda7be77de84476d03437ba2852df1
Pavol Humeny <pavol.humeny@gmail.com>
2025-11-02 14:30:45 +0100

    fix: Include visitsController

--- routes/visits.js
+++ routes/visits.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const eventController = require("../controllers/visitsController");
+const visitsController = require("../controllers/visitsController");
 
 router.get("/", visitsController.getVisitsByDay);
 router.get("/allVisits", visitsController.getAllVisitsCount);
 router.get("/uniqueVisits", visitsController.getUniqueVisitsCount);
-router.get(
-  "/lastSevenDaysVisits",
-  visitsController.getLastSevenDaysVisits
-);
+router.get("/lastSevenDaysVisits", visitsController.getLastSevenDaysVisits);
 router.get("/visitsByCountry", visitsController.getVisitsByCountry);
 
 module.exports = router;
