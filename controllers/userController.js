2364d2374f6f56ed34c4f68a618de3f1b9ef7f1d
Pavol Humeny <pavol.humeny@gmail.com>
2026-04-11 14:07:06 +0200

    feat: Add export success rate metric to user stats

--- controllers/userController.js
+++ controllers/userController.js
@@ -836,6 +836,21 @@ exports.getUserComparison = async (req, res) => {
           END
         ) AS eventsPerMinute
 
+        -- Export success rate (export / import)
+        (
+          CASE 
+            WHEN (SELECT COUNT(*) FROM events e 
+                  WHERE e.userId = u.userId AND e.eventType = 'uploadImage') > 0
+            THEN
+              (SELECT COUNT(*) FROM events e 
+                WHERE e.userId = u.userId AND e.eventType = 'exportImage')
+              /
+              (SELECT COUNT(*) FROM events e 
+                WHERE e.userId = u.userId AND e.eventType = 'uploadImage')
+            ELSE 0
+          END
+        ) AS exportRate
+
       FROM users u
       ${exclusionSql}
       `,
@@ -886,6 +901,7 @@ exports.getUserComparison = async (req, res) => {
         exportCount: buildMetric("exportCount"),
         sessionTimeTotal: buildMetric("sessionTimeTotal"),
         eventsPerMinute: buildMetric("eventsPerMinute"),
+        exportRate: buildMetric("exportRate"),
       },
     });
   } catch (err) {
